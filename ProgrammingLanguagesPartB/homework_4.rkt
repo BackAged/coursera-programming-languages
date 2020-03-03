b4e172c810af1c4b6173019b7c1f614a67e2bacd
ShahinMahmud <rsuitsit@gmail.com>
2020-03-03 17:45:25 +0600

    first week homework

--- ProgrammingLanguagesPartB/homework_4.rkt
+++ ProgrammingLanguagesPartB/homework_4.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(provide (all-defined-out))
+
 (define (sequence low high stride)
   (cond
     [(> low high) null]
@@ -7,10 +9,13 @@
 
 
 (define (string-append-map xs suffix)
-  (map (lambda (el) (string-append suffix el)) xs))
+  (map (lambda (el) (string-append el suffix)) xs))
 
 (define (list-nth-mod xs n)
-  xs)
+  (cond
+    [(negative? n) (error "list-nth-mod: negative number")]
+    [(empty? xs) (error "list-nth-mod: empty list")]
+    [else (car (list-tail xs (remainder n (length xs))))]))
 
 (define (stream-for-n-steps s n)
   (cond
@@ -19,8 +24,32 @@
      (let ([p (s)])
        (cons (car p) (stream-for-n-steps (cdr p) (- n 1))))])) 
 
-(define (funny-number-stream)
+(define funny-number-stream
   (letrec ([fdn (lambda (x) (if (zero? (modulo x 5)) (* -1 x) x ))]
            [f (lambda (x) (cons (fdn x) (lambda () (f (+ x 1)))))])
     (lambda () (f 1))))
 
+
+(define dan-then-dog
+  (letrec ([fdn (lambda (x) (if (string=? x "dan.jpg") "dog.jpg" "dan.jpg"))]
+           [f (lambda (x) (cons x (lambda () (f (fdn x)))))])
+    (lambda () (f "dan.jpg"))))
+
+(define (stream-add-zero s)
+  (letrec ([f (lambda (s) (lambda () (cons (cons 0 (car (s))) (f (cdr (s))))))])
+         (f s)))
+
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (x) (lambda () (cons (cons (list-nth-mod xs x) (list-nth-mod ys x)) (f (+ x 1)))))])
+    (f 0)))
+
+(define (vector-assoc v vec)
+  (let ([len (vector-length vec)])
+    (letrec ([f (lambda (pos)
+      (cond ((= pos len) #f)
+            ((pair? (vector-ref vec pos))
+                    (if (equal? (car (vector-ref vec pos)) v)
+                        (vector-ref vec pos)
+                        (f (+ pos 1))))
+            (#t (f (+ pos 1)))))])
+      (f 0))))
